51a10cb6b8e839e948bfbac9ef3d17177228e66c
MagM1go <68418026+MagM1go@users.noreply.github.com>
2026-06-12 12:51:58 +0700

    feat(backend): initialize S3 backend

---
+++ backend.tf
@@ -0,0 +1,13 @@
+terraform {
+  backend "s3" {
+    bucket    = "lab56eoooi"
+    key       = "lab56/terraform.tfstate"
+    endpoints = { s3 = "https://hb.ru-msk.vkcloud-storage.ru" }
+    region    = "ru-msk"
+
+    skip_region_validation      = true
+    skip_credentials_validation = true
+    skip_metadata_api_check     = true
+    skip_requesting_account_id  = true
+  }
+}
